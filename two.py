4fadb38a4f9b85420f84acc3ab2708b172a649c3
Stian A. Johansen <stian.uansett@gmail.com>
2013-03-26 20:36:13 +0000

    printing dates, cannot select

--- two.py
+++ two.py
@@ -5,15 +5,22 @@ endpoint = SPARQLWrapper("http://dbpedia.org/sparql")
 
 endpoint.setQuery("""
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+    PREFIX dbpp: <http://dbpedia.org/property/>
     PREFIX dbpr: <http://dbpedia.org/resource/>
     PREFIX dbpo: <http://dbpedia.org/ontology/>
+    PREFIX so: <http://schema.org/>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    
 
-    SELECT ?film_titl
+    SELECT DISTINCT ?film ?birthDate ?directorResource
         WHERE { 
-        ?film_titl rdf:type dbpo:Film .
-        } LIMIT 10 """)
+        ?film rdf:type dbpo:Film .
+        ?film dbpo:director ?directorResource .
+        ?directorResource rdfs:label "Quentin Tarantino"@en .
+        ?directorResource dbpp:birthDate ?birthDate .
+        } LIMIT 100 """)
 
 endpoint.setReturnFormat(JSON) 
 results = endpoint.query().convert()
 for res in results["results"]["bindings"] : 
-    print res['film_titl']['value'].encode('utf-8')
+    print res['birthDate']['value'].encode('utf-8')
