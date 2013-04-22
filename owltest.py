3572036120cb8bc429618ab3145a81a79edb36bd
Stian A. Johansen <stian.uansett@gmail.com>
2013-04-22 23:34:46 +0100

    final commit

--- owltest.py
+++ owltest.py
@@ -3,6 +3,7 @@ import rdflib
 from rdflib.graph import Graph,  Store,  URIRef,  Literal
 from rdflib.namespace import Namespace, RDF, RDFS
 from rdflib import plugin
+import sys
 # The Endpoint configuration
 endpoint = SPARQLWrapper("http://dbpedia.org/sparql")
 construct_query = '''
@@ -13,16 +14,18 @@ PREFIX dbpp: <http://dbpedia.org/property/>
 CONSTRUCT {
 ?film a qm:Film .
 ?film qm:isDirectedBy ?director .
-?film dbpp:name ?title .
 ?director a qm:Director .
-?film qm:hasBudget ?budget . 
+?film qm:hasBudget ?budget .
+?film qm:isDistributedBy ?distributor .
+?distributor a qm:Organisation .
+
 }
 WHERE{
 ?film a dbpo:Film . 
 ?film dbpo:director ?director .
-?film dbpp:name ?title .
 ?director a dbpo:Agent . 
-?film dbpo:budget ?budget . 
+?film dbpo:budget ?budget .
+?film dbpo:distributor ?distributor .
 } LIMIT 1000
 
 '''
@@ -71,28 +74,38 @@ g.parse('Frank_Darabont.n3', format='turtle')
 g.parse('Grammy_Award_winners.n3', format='turtle') 
 g.parse(data=rdf_xml_data, format='turtle')
 
+def runQuery(customQuery=""):
+    query = """
+    PREFIX qm: <http://qmul.ac.uk/sw/g7/ontology#>
+    PREFIX dbprc: <http://dbpedia.org/resource/Category:>
+    PREFIX purl: <http://purl.org/dc/terms/>
+    PREFIX dbpo: <http://dbpedia.org/ontology/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX dbpd: <http://dbpedia.org/datatype/>
+    PREFIX dbpp: <http://dbpedia.org/property/>
 
-query = """
-PREFIX qm: <http://qmul.ac.uk/sw/g7/ontology#>
-PREFIX dbprc: <http://dbpedia.org/resource/Category:>
-PREFIX purl: <http://purl.org/dc/terms/>
-PREFIX dbpo: <http://dbpedia.org/ontology/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX dbpd: <http://dbpedia.org/datatype/>
-PREFIX dbpp: <http://dbpedia.org/property/>
-
-SELECT ?film ?budget ?director WHERE {
+    SELECT ?film ?budget ?director ?organisation WHERE {
         ?film a qm:Film .
         ?film qm:hasBudget ?budget .
         ?film qm:isDirectedBy ?director .
+        ?film qm:isDistributedBy ?organisation .
         ?director purl:subject dbprc:Grammy_Award_winners .
-}
-"""
-for row in g.query(query):
-    print row
+    }
+    """
+    if(customQuery  ==  "i"):
+        query = open('input.txt', 'r').read()
+        print query
 
-#for ns in g.namespaces():
-#    print "Prefix: %s => URI: %s" %ns
+    for row in g.query(query):
+        print row
 
-#for film in g.subjects(object=URIRef("http://qmul.ac.uk/sw/g7/ontology#Film"),  predicate=None):
-#    print film
+
+def main():
+    print("Welcome. Type i for using the input.txt file for custom queries or just enter for a pre-made query showing films with budget and organisation made by a director who also got a Grammy Award") 
+    queryv = raw_input("i or blank: ")
+    runQuery(queryv)
+    
+
+
+
+if  __name__ =='__main__':main()
